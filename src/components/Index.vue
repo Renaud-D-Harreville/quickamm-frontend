af234d6140ea411a2a76753561d453be3c22dfd5
Renauddha <r.dharreville@gmail.com>
2025-11-12 15:57:51 +0100

    Removing unused CSS

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -12,33 +12,9 @@ import Mcq from './Mcq.vue';
 </template>
 
 <style scoped>
-
-
       @media (max-width: 320px) {
           .element {
               width: 90%;
           }
-      }
-
-      #fixed-row .correct {
-          color: darkseagreen;
-          opacity: 1;
-      }
-
-       #fixed-row .wrong {
-           color: firebrick;
-           opacity: 1;
-       }
- /* Adjusting the two boxes so that they are at the same horizontal height */
-      #fixed-row .col-10,
-      #fixed-row .col-2 {
-          display: flex; /* Active le mode Flexbox pour les deux colonnes */
-          align-items: center; /* Centrage vertical parfait */
-          height: 100%; /* Uniformise la hauteur */
-      }
-
-      #fixed-row button {
-          margin: 0; /* Supprime les marges par défaut */
-          transform: translateY(-0px); /* Ajuster légèrement si c'est encore désaligné */
       }
   </style>
